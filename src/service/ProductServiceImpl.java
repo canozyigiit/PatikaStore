31e7e00918e253670b69e2b1e6d8a7618a1e2276
canozyigiit <muhammetcanozyigit@gmail.com>
2022-11-24 18:40:27 +0300

    updated

--- src/service/ProductServiceImpl.java
+++ src/service/ProductServiceImpl.java
@@ -98,31 +98,4 @@ public class ProductServiceImpl implements ProductService {
     }
 
 
-    private static void tableFormat( String[][] table) {
-        boolean leftJustifiedRows = false;
-
-        Map<Integer, Integer> columnLengths = new HashMap<>();
-        Arrays.stream(table).forEach(a -> Stream.iterate(0, (i -> i < a.length), (i -> ++i)).forEach(i -> {
-            if (columnLengths.get(i) == null) {
-                columnLengths.put(i, 0);
-            }
-            if (columnLengths.get(i) < a[i].length()) {
-                columnLengths.put(i, a[i].length());
-            }
-        }));
-
-        /*
-         * Prepare format String
-         */
-        final StringBuilder formatString = new StringBuilder("");
-        String flag = leftJustifiedRows ? "-" : "";
-        columnLengths.entrySet().stream().forEach(e -> formatString.append("| %" + flag + e.getValue() + "s "));
-        formatString.append("|\n");
-
-        /*
-         * Print table
-         */
-        Stream.iterate(0, (i -> i < table.length), (i -> ++i))
-                .forEach(a -> System.out.printf(formatString.toString(), table[a]));
-    }
 }
